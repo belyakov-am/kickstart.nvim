b24e5eb68e3e0475c9c6e3a7c47ccc82d2fe768d
Artem Belyakov <artm.belyakov@gmail.com>
2023-10-27 13:12:39 +0200

    enable copilot for all files

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -39,7 +39,7 @@ return {
         hgcommit = false,
         svn = false,
         cvs = false,
-        ['.'] = false,
+        ['.'] = true,
       },
       copilot_node_command = 'node', -- Node.js version must be > 16.x
       server_opts_overrides = {},
