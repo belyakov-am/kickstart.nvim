e9f799754db989862cb223f05eb4698ff0d935ed
Artem Belyakov <artm.belyakov@gmail.com>
2024-02-06 17:44:09 +0100

    feat: adapt gitsigns keybindings

--- init.lua
+++ init.lua
@@ -120,7 +120,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -164,25 +164,27 @@ require('lazy').setup({
         end, { expr = true, desc = 'Jump to previous hunk' })
 
         -- Actions
-        -- visual mode
+        -- Visual mode
         map('v', '<leader>hs', function()
           gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'stage git hunk' })
         map('v', '<leader>hr', function()
           gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'reset git hunk' })
-        -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
-          gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
+        -- Normal mode
+        -- Hunk
+        map('n', '<leader>ghs', gs.stage_hunk, { desc = '[G]it [H]unk [S]tage' })
+        map('n', '<leader>ghr', gs.reset_hunk, { desc = '[G]it [H]unk [R]eset' })
+        map('n', '<leader>ghu', gs.undo_stage_hunk, { desc = '[G]it [H]unk [U]ndo stage' })
+        map('n', '<leader>ghp', gs.preview_hunk, { desc = '[G]it [H]unk [P]review' })
+
+        -- Buffer
+        map('n', '<leader>gbs', gs.stage_buffer, { desc = '[G]it [B]uffer [S]tage' })
+        map('n', '<leader>gbr', gs.reset_buffer, { desc = '[G]it [Buffer] [R]eset' })
+
+        -- Diff
+        map('n', '<leader>ghd', gs.diffthis, { desc = 'git diff against index' })
+        map('n', '<leader>ghD', function()
           gs.diffthis '~'
         end, { desc = 'git diff against last commit' })
 
@@ -410,23 +412,23 @@ vim.g.loaded_python3_provider = 0
 -- Catpuccin
 require('catppuccin').setup {
   flavour = 'mocha', -- latte, frappe, macchiato, mocha
-  background = { -- :h background
+  background = {     -- :h background
     light = 'latte',
     dark = 'mocha',
   },
   transparent_background = false, -- disables setting the background color.
-  show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-  term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+  term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
   dim_inactive = {
-    enabled = false, -- dims the background color of inactive window
+    enabled = false,              -- dims the background color of inactive window
     shade = 'dark',
-    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    percentage = 0.15,            -- percentage of the shade to apply to the inactive window
   },
-  no_italic = false, -- Force no italic
-  no_bold = false, -- Force no bold
-  no_underline = false, -- Force no underline
-  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { 'italic' }, -- Change the style of comments
+  no_italic = false,              -- Force no italic
+  no_bold = false,                -- Force no bold
+  no_underline = false,           -- Force no underline
+  styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { 'italic' },      -- Change the style of comments
     conditionals = { 'italic' },
     loops = {},
     functions = {},
@@ -534,7 +536,8 @@ vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find, { desc = '[/] Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>/', require('telescope.builtin').current_buffer_fuzzy_find,
+  { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>fG', require('telescope.builtin').git_files, { desc = '[F]ind [G]it Files' })
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
