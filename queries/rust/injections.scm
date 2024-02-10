bd294a930056c22210302de338fc2595e4e24277
Artem Belyakov <artm.belyakov@gmail.com>
2024-02-10 13:39:14 +0200

    feat: improve rust sqlx treesitter

--- queries/rust/injections.scm
+++ queries/rust/injections.scm
@@ -8,6 +8,22 @@
     (raw_string_literal) @injection.content
   ) 
 
-  (#offset! @injection.content 1 0 0 0)
+  ; Trim leading `r#"` and trailing `"#`.
+  (#offset! @injection.content 0 3 0 -2)
+  (#set! injection.language "sql")
+)
+
+(macro_invocation
+  (scoped_identifier
+    path: (identifier) @_path (#eq? @_path "sqlx")
+    name: (identifier) @_name (#any-of? @_name "query" "query_as" "query_scalar")
+  )
+
+  (token_tree
+    (string_literal) @injection.content
+  )
+
+  ; Trim leading `"` and trailing `"`.
+  (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "sql")
 )
