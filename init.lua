474b057c159be7040049ca4f504249d35259c3f9
Artem Belyakov <artm.belyakov@gmail.com>
2024-11-16 12:01:02 +0200

    feat: Disable pyright features in favor of ruff

--- init.lua
+++ init.lua
@@ -877,7 +877,29 @@ local servers = {
     },
   },
 
-  ruff_lsp = {},
+  ruff_lsp = {
+    init_options = {
+      settings = {
+        -- Format on save
+        format = {
+          args = {},
+        },
+      },
+    },
+  },
+
+  pyright = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
 }
 
 -- Setup neovim lua configuration
