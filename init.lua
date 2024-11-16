8ac0bfd6508bf0e6e46c509e84f0a2809a7be825
Artem Belyakov <artm.belyakov@gmail.com>
2024-11-16 12:00:17 +0200

    feat: Add keybind for formatting

--- init.lua
+++ init.lua
@@ -811,6 +811,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+  nmap('<leader>cf', ':Format<CR>', 'Format current buffer with LSP')
 end
 
 -- document existing key chains
